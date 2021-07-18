3707e46b03f877458fb5bc100dd35f087aca7ed2
Arpit Dixit <arpitd@dewsolutions.in>
2021-07-18 17:23:36 +0530

    Initial Commit

---
+++ SwiftUI_Forms/SwiftUI_FormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FormsApp.swift
+//  SwiftUI_Forms
+//
+//  Created by Arpit Dixit on 18/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
